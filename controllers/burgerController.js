662a74a5dbf6ac773caf1bf0665a8304bb7d7096
Sitwat <sitwat.nadeem1@gmail.com>
2018-09-04 19:38:37 -0400

    controller

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -1,3 +1,19 @@
 var express = require("express");
 var router = express.Router();
-var db = require("../models/burgerDB");
+var db = require("../models/burger");
+
+
+
+router.get("/", function(req, res){
+
+    db.findAll({}).then(function(results) {
+        // results are available to us inside the .then
+       // console.log(results);
+        res.render("index", {buns: results}, {meats: results}, {cheeses: results}, {veggies: results}, {sides:results}, {drinks:results},{add_on:results}); 
+
+    });
+
+});
+
+
+module.exports = router;
