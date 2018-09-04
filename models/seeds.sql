af8c0801bf0c2840ed8dc032fb899abba6e080fa
Gina Paik <ginapaik@outlook>
2018-09-04 19:54:19 -0400

    updated all tables in database with chosen column

--- models/seeds.sql
+++ models/seeds.sql
@@ -1,70 +1,68 @@
-INSERT INTO buns (product, price, image)
-VALUES  ('Brioche', 0.00, '\public\images\brioche.png'),
-        ('Sesame', 0.00, '\public\images\sesame_bun.png'),
-        ('Wheat', 0.00, '\public\images\wheat_bun.png'),
-        ('Gluten Free', 0.00, '\public\images\gluten_free.png'),
-        ('Lettuce Wrap', 0.00, '\public\images\lettuce.png');
+INSERT INTO buns (product, price, image, chosen)
+VALUES  ('Brioche', 0.00, '\public\images\brioche.png', FALSE),
+        ('Sesame', 0.00, '\public\images\sesame_bun.png', FALSE),
+        ('Wheat', 0.00, '\public\images\wheat_bun.png', FALSE),
+        ('Gluten Free', 0.00, '\public\images\gluten_free.png', FALSE),
+        ('Lettuce Wrap', 0.00, '\public\images\lettuce.png', FALSE);
 
-INSERT INTO cheeses (product, price, image)
-VALUES  ()
+INSERT INTO cheeses (product, price, image, chosen)
+VALUES  ('American', 0.75, '\public\images\swisscheese.png', FALSE),
+        ('Provolone', 0.75, '\public\images\swisscheese.png', FALSE),
+        ('Cheddar', 0.75, '\public\images\cheddarcheese.png', FALSE),
+        ('Gouda', 0.75, '\public\images\goudacheese.png', FALSE),
+        ('Feta', 0.75, '\public\images\swisscheese.png', FALSE),
+        ('Swiss', 0.75, '\public\images\swisscheese.png', FALSE),
+        ('Pepper Jack', 0.75, '\public\images\swisscheese.png', FALSE),
+        ('Blue Cheese', 0.75, '\public\images\bluecheese.png', FALSE);
 
-        
--- 		('Ketchup', 'Condiments', 0.25),
---         ('Mayo', 'Condiments', 0.25),
---         ('Mustard', 'Condiments', 0.25),
---         ('BBQ', 'Condiments', 0.25),
---         ('Ranch', 'Condiments', 0.25),
---         ('Chipotle Mayo', 'Condiments', 0.25),
---         ('Sriracha', 'Condiments', 0.25),
---         ('Duck Sauce', 'Condiments', 0.25),
---         ('Sweet & Sour', 'Condiments', 0.25),
--- 		('American', 'Cheese', 0.75),
---         ('Provolone', 'Cheese', 0.75),
---         ('Cheddar', 'Cheese', 0.75),
---         ('Gouda', 'Cheese', 0.75),
---         ('Feta', 'Cheese', 0.75),
---         ('Swiss', 'Cheese', 0.75),
---         ('Pepper Jack', 'Cheese', 0.75),
---         ('Blue Cheese', 'Cheese', 0.75),
---         ('Bell Pepper', 'Veggies', 0.50),
---         ('Lettuce', 'Veggies', 0.50),
---         ('Onion', 'Veggies', 0.50),
---         ('Tomato', 'Veggies', 0.50),
---         ('Banana Peppers', 'Veggies', 0.50),
---         ('Pickles', 'Veggies', 0.50),
---         ('Mushrooms', 'Veggies', 0.50),
---         ('Arugula', 'Veggies', 0.50),
---         ('Bean Sprouts', 'Veggies', 0.50),
---         ('Kale', 'Veggies', 0.50),
---         ('Spinach', 'Veggies', 0.50),
--- 		('Avocado', 'Add Ons', 2.00),
---         ('Lobster', 'Add Ons', 2.00),
---         ('Shrimp', 'Add Ons', 2.00),
---         ('Bacon', 'Add Ons', 2.00),
---         ('Fried Egg', 'Add Ons', 2.00),
---         ('Pepperoni', 'Add Ons', 2.00),
---         ('Pineapple', 'Add Ons', 2.00),
---         ('Strawberries', 'Add Ons', 2.00),
---         ('Peanut Butter', 'Add Ons', 2.00),
---         ('Anchovies', 'Add Ons', 2.00),
---         ('Captn Crunch', 'Add Ons', 2.00),
--- 		('Fruit', 'Sides', 3.00),
---         ('Mashed Potatoes', 'Sides', 3.00),
---         ('French Fries', 'Sides', 3.00),
---         ('Cole Slaw', 'Sides', 3.00),
---         ('Mixed Veggies', 'Sides', 3.00),
---         ('Tater Tots', 'Sides', 3.00),
--- 		('Coke', 'Drinks', 1.50),
---         ('Diet Coke', 'Drinks', 1.50),
---         ('Lemonade', 'Drinks', 1.50),
---         ('Sprite', 'Drinks', 1.50),
---         ('Iced Tea', 'Drinks', 1.50),
---         ('Smartest Water', 'Drinks', 1.50);
+INSERT INTO meats (product, price, image, chosen)
+VALUES  ('Beef', 5.00, '\public\images\swisscheese.png', FALSE),
+        ('Chicken', 5.00, '\public\images\swisscheese.png', FALSE),
+        ('Turkey', 5.00, '\public\images\swisscheese.png', FALSE),
+        ('Veggie', 5.00, '\public\images\swisscheese.png', FALSE);
 
+INSERT INTO veggies (product, price, image, chosen)
+VALUES  ('Bell Pepper', 0.50, '\public\images\lettuce.png', FALSE),
+        ('Lettuce', 0.50, '\public\images\lettuce.png', FALSE),
+        ('Onion', 0.50, '\public\images\onion.png', FALSE),
+        ('Tomato', 0.50, '\public\images\tomato.png', FALSE),
+        ('Banana Peppers', 0.50, '\public\images\lettuce.png', FALSE),
+        ('Pickles', 0.50, '\public\images\pickle.png', FALSE),
+        ('Mushrooms', 0.50, '\public\images\mushroom.png', FALSE),
+        ('Arugula', 0.50, '\public\images\lettuce.png', FALSE),
+        ('Bean Sprouts', 0.50, '\public\images\lettuce.png', FALSE),
+        ('Kale', 0.50, '\public\images\lettuce.png', FALSE),
+        ('Spinach', 0.50, '\public\images\lettuce.png', FALSE);
 
+INSERT INTO add_ons (product, price, image, chosen)
+VALUES 	('Avocado', 2.00, '\public\images\lettuce.png', FALSE),
+        ('Lobster', 2.00, '\public\images\lobster.png', FALSE),
+        ('Shrimp', 2.00, '\public\images\shrimp.png', FALSE),
+        ('Bacon', 2.00, '\public\images\bacon.png', FALSE),
+        ('Fried Egg', 2.00, '\public\images\egg.png', FALSE),
+        ('Peanut Butter', 2.00, '\public\images\peanutbutter.png', FALSE);
 
+INSERT INTO sides (product, price, image, chosen)
+VALUES  ('Fruit', 3.00, '\public\images\shrimp.png', FALSE),
+        ('Mashed Potatoes', 3.00, '\public\images\shrimp.png', FALSE),
+        ('French Fries', 3.00, '\public\images\shrimp.png', FALSE),
+        ('Cole Slaw', 3.00, '\public\images\shrimp.png', FALSE),
+        ('Mixed Veggies', 3.00, '\public\images\shrimp.png', FALSE),
+        ('Tater Tots', 3.00, '\public\images\shrimp.png', FALSE);
 
--- ('Beef', 5.00, ),
---         ('Chicken', 'Meats', 5.00),
---         ('Turkey', 'Meats', 5.00),
---         ('Veggie', 'Meats', 5.00),
+INSERT INTO drinks (product, price, image, chosen)
+VALUES 	('Coke', 1.50, '\public\images\shrimp.png', FALSE),
+        ('Diet Coke', 1.50, '\public\images\shrimp.png', FALSE),
+        ('Lemonade', 1.50, '\public\images\shrimp.png', FALSE),
+        ('Sprite', 1.50, '\public\images\shrimp.png', FALSE),
+        ('Iced Tea', 1.50, '\public\images\shrimp.png', FALSE);
+
+INSERT INTO condiments (product, price, image, chosen)
+VALUES  ('Ketchup', 0.25, '\public\images\ketchup.png', FALSE),
+        ('Mayo', 0.25, '\public\images\mayo.png', FALSE),
+        ('Mustard', 0.25, '\public\images\mustard.png', FALSE),
+        ('BBQ', 0.25, '\public\images\bbq.png', FALSE),
+        ('Ranch', 0.25, '\public\images\ranch.png', FALSE),
+        ('Chipotle Mayo', 0.25, '\public\images\chipotle_mayo.png', FALSE),
+        ('Sriracha', 0.25, '\public\images\sriracha.png', FALSE);
+
