7c31027d36553e993148d9b6a4583bd9e48058b3
Nicole Richardson <njrichardson95@gmail.com>
2018-09-06 23:16:12 -0400

    fixed changes and added in sitwats content

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 var sequelize = require("sequelize");
 
-var sequelize = new sequelize("burger_bitdb", "root", "gwbootcamp",{
+var sequelize = new sequelize("burger_bitdb", "root", "1875Njr1875",{
     host: "localhost",
     port: 3306,
     dialect: "mysql",
-    
+
         pool:{
             max: 5,
             min: 0,
