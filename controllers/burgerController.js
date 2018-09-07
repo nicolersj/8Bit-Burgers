e999cdd5c6f680e5ddff715dfe8a551cd950fdba
Nicole Richardson <njrichardson95@gmail.com>
2018-09-07 00:35:49 -0400

    added in additional models files have all images rendering now

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -2,24 +2,79 @@ var express = require("express");
 var router = express.Router();
 var BunsDB = require("../models/BunsDB");
 var CheesesDB = require("../models/CheesesDB");
+var AddOnsDB = require("../models/AddOnsDB");
+var CondimentsDB = require("../models/CondimentsDB");
+var DrinksDB = require("../models/DrinksDB");
+var MeatsDB = require("../models/MeatsDB");
+var SidesDB = require("../models/SidesDB");
+var VeggiesDB = require("../models/VeggiesDB");
 
 
 
 router.get("/", function(req, res){
-    
-    var data = {buns: {}, cheeses: {}};
+
+    var data = {buns: {}, cheeses: {}, add_ons: {}, condiments: {}, drinks: {}, meats: {}, sides: {}, veggies: {}};
     BunsDB.findAll({}).then(function(results) {
 
         console.log(results);
      data.buns =  results
     });
-    
+
 
     CheesesDB.findAll({}).then(function(results) {
 
         console.log(results);
         data.cheeses = results
-    
+
+    });
+
+    AddOnsDB.findAll({}).then(function(results) {
+
+        console.log(results);
+        data.add_ons = results
+
+    });
+
+    CondimentsDB.findAll({}).then(function(results) {
+
+        console.log(results);
+        data.condiments = results
+
+    });
+
+    DrinksDB.findAll({}).then(function(results) {
+
+        console.log(results);
+        data.drinks = results
+
+    });
+
+    DrinksDB.findAll({}).then(function(results) {
+
+        console.log(results);
+        data.drinks = results
+
+    });
+
+    MeatsDB.findAll({}).then(function(results) {
+
+        console.log(results);
+        data.meats = results
+
+    });
+
+    SidesDB.findAll({}).then(function(results) {
+
+        console.log(results);
+        data.sides = results
+
+    });
+
+    VeggiesDB.findAll({}).then(function(results) {
+
+        console.log(results);
+        data.veggies = results
+
     });
 
     res.render("index", {data: data});
